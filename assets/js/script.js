240846ebba71de00fee425f5a7639072a58f51ff
Dave May <echo.dave@protonmail.com>
2019-10-31 01:06:12 -0400

    expanded on some comments in trying to debug

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             ["Allie McBeal", "Tom Clancy", "Heraldo Riviera", "Shakespear"],
             [13, 50, 49, 47],
             [1867, 1994, 1776, 1819],
-            ["Edison", "Bell","Galileo", "Da Vinci"]
+            ["Edison", "Bell", "Galileo", "Da Vinci"]
         ],
         Answers: [3, 1, 0, 1]
 
@@ -24,11 +24,11 @@ $(document).ready(function () {
 
     //timer display counter
     let counter = 0;
-    
+
     //question builder
     function startQuestions() {
         $('.question').html(qaObject.questions[qCount]);
-            $('li').remove();
+        $('li').remove();
         for (let i = 0; i < qaObject.options[qCount].length; i++) {
             $('ul.options').append(`<li value="${i}">${qaObject.options[qCount][i]}</li>`)
         }
@@ -38,12 +38,15 @@ $(document).ready(function () {
         questionTimer()
         countDown();
         $('li').on("click", function () {
+            // clearInterval(qTimer - question timer 30s);
+            clearInterval(timerCounter);
+
             console.log($(this).attr("value"));
-            if ($(this).attr("value") == answer)  {
+            if ($(this).attr("value") == answer) {
                 $(this).addClass("answer");
                 correctCount = correctCount + 1;
                 answerTimer();
-            } 
+            }
             else {
                 $(this).addClass("wrong");
                 wrongCount += 1;
@@ -51,65 +54,73 @@ $(document).ready(function () {
                 $('li').off();
                 answerTimer();
             }
-            // clearInterval(qTimer);
-             clearInterval(timerCounter);
 
 
         });
 
-        
+
     }
 
     //question timer
     function questionTimer() {
         qTimer = setTimeout(timeout, 30000);
     }
-        function timeout() {
-            wrongCount += 1;
-            // some how this call add isn't working anymore
-            $(`li[value="${answer}"]`).addClass("answer");
+    function timeout() {
+        wrongCount += 1;
+        $(`li[value="${answer}"]`).addClass("answer");
 
-            // clearInterval(qTimer);
-            console.log("wrong " + wrongCount);
-            answerTimer();
-            
+     
+        console.log("wrong " + wrongCount);
+        answerTimer();
 
-        }
+    }
 
-    
-//10s timer after wrong answer
+
+    //3s timer after wrong answer
     function answerTimer() {
-        if (qCount <  qaObject.options.length) {
+        if (qCount < qaObject.options.length) {
+            console.log("length " + qaObject.options.length);
             aTimer = setTimeout(startQuestions, 3000);
         }
         else {
-            $('.wrap').prepend(`<h1>correct: ${correctCount}, wrong: ${wrongCount}</ha>`)
+            $('.wrap').prepend(`<h1>correct: ${correctCount}, wrong: ${wrongCount}</h1>`)
+
+            //restart button
+            // $('#restart').css("display", "inline-block");
+            // $('#restart').on("click", resetGame());
         }
     }
-    
+
     $('#start').on('click', function () {
 
         startQuestions();
-        questionTimer;
 
         $('#start').off();
-        $('#start').css("display","none");
+        $('#start').css("display", "none");
     });
 
-//timer display
-function countDown() {
-    timerCounter = setInterval(count, 1000);
+    //timer display
+    function countDown() {
+        timerCounter = setInterval(count, 1000);
 
-}
-//timer display decrement
-function count() {
-    counter -= 1;
-    $('#count').html(counter);
-    if (counter <= 0) {
-        clearInterval(timerCounter)
     }
-}
-    
+    //timer display decrement
+    function count() {
+        counter -= 1;
+        $('#count').html(counter);
+        if (counter <= 0) {
+            clearInterval(timerCounter)
+        }
+    }
 
+    //currently disabled on lines 90-91
+    function resetGame() {
+        let correctCount = 0;
+        let wrongCount = 0;
+        let qCount = 0;
+        let answer = "";
+        let counter = 0;
+        startQuestions();
+    }
 
 });
